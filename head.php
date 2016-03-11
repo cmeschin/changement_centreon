8f80c495835ec51bc294cd4f77fbe001712958e6
cmeschin <cedricmeschin@gmail.com>
2016-03-11 09:56:19 +0100

    merge

--- head.php
+++ head.php
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-
-	<meta charset="utf-8" />
-	<meta http-equiv="cache-control" content="max-age=3600" /> <!-- conserve le cache pendant une heure maximum-->
-<!--	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />	-->
-<!--	<link href="CalendarControl.css"  rel="stylesheet" type="text/css"> -->
-	<title>DEV - Changement Centreon - Tessi Technologies</title>
-<!-- 	<link href="css/tessi-theme/jquery-ui-1.10.4.custom.css" rel="stylesheet" type="text/css" /> -->
-	<link href="css/tessi-theme/jquery-ui-1.10.4.custom.min.css" rel="stylesheet" type="text/css" />
-	<link href="css/style.css" rel="stylesheet" type="text/css"/>
-	<link href="css/cleanslate.css" rel="Stylesheet" type="text/css"/>
- 	<link href="css/jquery.weekLine.css" rel="Stylesheet" type="text/css"/>
-<!-- 	<link href="css/jquery.timepicker.css" rel="Stylesheet" type="text/css"/> -->
-	<link href="css/jquery.datetimepicker.css" rel="Stylesheet" type="text/css"/>
-	<link rel="icon" href="./images/Tessi_Icone.png" />
-=======
 <meta charset="utf-8" />
 <meta http-equiv="cache-control" content="max-age=3600" /> <!-- conserve le cache pendant une heure maximum-->
 <!-- <META HTTP-EQUIV="refresh" CONTENT="60"> <!-- rafraichit automatiquement la page toutes les X secondes -->
@@ -24,7 +8,6 @@
 <link href="css/jquery.weekLine.css" rel="Stylesheet" type="text/css"/>
 <link href="css/jquery.datetimepicker.css" rel="Stylesheet" type="text/css"/>
 <link rel="icon" href="./images/favicon.ico" />
->>>>>>> 1c8b103319679a4d3c954f77d8410b9feb3b8d76
 	
 <style>
 	.custom-combobox {
