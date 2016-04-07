69cfb955c08a9e32c1ef5ffed87bc3700ad29e9e
cmeschin <cedricmeschin@gmail.com>
2016-04-07 15:35:50 +0200

    2016-04-07 9.4.0 	suppression du lien cliquable si demande en cours

--- menu.php
+++ menu.php
@@ -7,9 +7,9 @@ if (file_exists('maintenance.php')==False)
 		echo '<li><a href="nouvelle_demande.php">Formuler une demande</a></li>';
 		if (($_SESSION['Nouveau']== true) OR ($_SESSION['Reprise']== true))
 		{
-			echo '<li><a href="#">Lister les demandes</a></li>';
-			echo '<li><a href="#">Extraire une prestation</a></li>';
-			echo '<li><a href="#">Documentation</a></li>';
+			echo '<li><a >Lister les demandes</a></li>';
+			echo '<li><a >Extraire une prestation</a></li>';
+			echo '<li><a >Documentation</a></li>';
 		} else 
 		{
 			echo '<li><a href="lister_demande.php">Lister les demandes</a></li>';
