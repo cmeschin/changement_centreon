53823ccc142b25cbb9b0dfe2f34697de46c95633
cmeschin <cedricmeschin@gmail.com>
2017-08-29 14:59:07 +0200

    2017-08-29 9.13.0

--- information.php
+++ information.php
@@ -8,6 +8,10 @@
  */
 
 $balise= "h3";
+$balise2= "h3";
 $class_message = "attention";
+$class_message2 = "attention";
 $message = "A compter du 3 Octobre 2016, les brouillons supérieurs à trois mois seront automatiquement supprimés.";
-echo "<" . $balise . " class='" . $class_message . "'>" . $message . "</" . $balise . ">";
+$message2 = "Une mise à jour a été effectuée le 29 Aout à 15h; pensez à vider le cache de votre navigateur si vous ne l'avez pas déjà fait.";
+//echo "<" . $balise . " class='" . $class_message . "'>" . $message . "</" . $balise . ">";
+echo "<" . $balise2 . " class='" . $class_message2 . "'>" . $message2 . "</" . $balise2 . ">";
