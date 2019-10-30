22d252fe7c95562b3493c02a4ca182bf839ad8f4
cmeschin <cedricmeschin@gmail.com>
2019-10-30 16:04:19 +0100

    Ajout message info

--- information.php
+++ information.php
@@ -9,9 +9,9 @@
 
 $balise= "h3";
 $balise2= "h3";
-$class_message = "attention";
+$class_message = "ok";
 $class_message2 = "attention";
-$message = "A compter du 3 Octobre 2016, les brouillons supérieurs à trois mois seront automatiquement supprimés.";
-$message2 = "Une mise à jour a été effectuée le 29 Aout à 15h; pensez à vider le cache de votre navigateur si vous ne l'avez pas déjà fait.";
-//echo "<" . $balise . " class='" . $class_message . "'>" . $message . "</" . $balise . ">";
+$message = "A compter du 1 Décembre 2019, les brouillons non modifiés supérieurs à deux mois seront automatiquement supprimés.";
+$message2 = "Une mise à jour a été effectuée le 30 Octobre; pensez à vider le cache de votre navigateur si vous ne l'avez pas déjà fait.";
+echo "<" . $balise . " class='" . $class_message . "'>" . $message . "</" . $balise . ">";
 echo "<" . $balise2 . " class='" . $class_message2 . "'>" . $message2 . "</" . $balise2 . ">";
